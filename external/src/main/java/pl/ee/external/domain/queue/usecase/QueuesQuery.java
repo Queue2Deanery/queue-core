ec8f2d6a5bb0c2853542bc5c4febe9a81572d9da
Krzysiek <50wilk>
2019-06-08 22:01:04 +0200

    QueuesQuery logic implemented

--- external/src/main/java/pl/ee/external/domain/queue/usecase/QueuesQuery.java
+++ external/src/main/java/pl/ee/external/domain/queue/usecase/QueuesQuery.java
@@ -2,15 +2,31 @@ package pl.ee.external.domain.queue.usecase;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.ee.common.model.QueueEntity;
 import pl.ee.external.domain.queue.dto.QueuesResponse;
+import pl.ee.external.infrastructure.repository.QueueRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class QueuesQuery {
 
-  public QueuesResponse logic() {
+  private QueueRepository queueRepository;
 
-    return null;
+  public QueuesResponse logic() {
+    List<QueueEntity> queueEntities = queueRepository.findAll();
+    List<QueuesResponse.ShortQueueResponse> shortQueueResponses = queueEntities.stream()
+            .map(queueEntity ->
+                    QueuesResponse.ShortQueueResponse.builder()
+                            .id(queueEntity.getId())
+                            .name(queueEntity.getName())
+                            .registrationEnd(queueEntity.getRegistrationEnd())
+                            .registrationStart(queueEntity.getRegistrationStart())
+                            .build())
+            .collect(Collectors.toList());
+    return QueuesResponse.builder().queues(shortQueueResponses).build();
   }
 
 }
